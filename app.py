57140681539e03d6749befdf14848fda9b6916bc
Stalin Vasco <stalinvasco.proyectos@gmail.com>
2025-05-08 11:33:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -293,4 +293,6 @@ def evaluate_response():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
